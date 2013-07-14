4d8629b990f20ccde13f496f663db9ee9f59a83c
antoniespinosa <antoniespinosa@me.com>
2013-07-14 19:39:02 +0200

    Mensajes 1.3

--- mensajes/views.py
+++ mensajes/views.py
@@ -20,13 +20,20 @@ def escribir(request):
     return render_to_response('escribir_mensaje.html', context, context_instance=RequestContext(request))  # Create your views here.
 @login_required
 def inbox(request):
-    lista_mensajes=Mensajes.objects.raw("SELECT * FROM Mensajes WHERE destinatario_id="+str(request.user.id)+" OR remitente_id="+str(request.user.id)+";")
     subconsulta1="SELECT remitente_id FROM Mensajes WHERE destinatario_id="+str(request.user.id)
     subconsulta2="SELECT destinatario_id FROM Mensajes WHERE remitente_id="+str(request.user.id)
     consulta="SELECT * FROM auth_user WHERE id IN ("+subconsulta1+") OR id IN ("+subconsulta2+");"
     usuarios=User.objects.raw(consulta)
-    formulario=False
-    context = {"lista_mensajes": lista_mensajes, "usuarios":usuarios, "pagina":"misMensajes", "formulario":formulario, "usuario":request.user}
+    conversador=get_object_or_404(User, pk=usuarios[0].id)
+    lista_mensajes=Mensajes.objects.raw("SELECT * FROM Mensajes WHERE (destinatario_id="+str(request.user.id)+" AND remitente_id="+str(conversador.id)+") OR (remitente_id="+str(request.user.id)+" AND destinatario_id="+str(conversador.id)+") ORDER BY id DESC;")
+
+    if request.POST:
+        formulario = Mandar_Mensaje_Form(request.POST, user=request.user, destinatario=conversador)
+        if formulario.is_valid:
+            formulario.save()
+    else:
+        formulario = Mandar_Mensaje_Form(instance=request.user.usuario, user=request.user, destinatario=conversador)
+    context = {"mensajes": lista_mensajes, "pagina":"misMensajes", "usuarios":usuarios, "formulario":formulario, "usuario":request.user}
     return render_to_response('mensajes/inbox.html', context, context_instance=RequestContext(request))
 def chat(request, user_id):
     conversador=get_object_or_404(User, pk=user_id)
