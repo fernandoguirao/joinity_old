c33781dc4b978f963d03e630f04191c197103254
antoniespinosa <antoniespinosa@me.com>
2013-07-16 17:07:00 +0200

    Estructura Static

--- usuario/views.py
+++ usuario/views.py
@@ -139,8 +139,8 @@ def editar_perfil(request):
     context={ 'user_form': user_form, 'perfil_form': perfil_form, "pagina":"editar" }
     return render_to_response('usuario/perfil.html', context, context_instance=RequestContext(request))
 def solicitar(request):
-    if request.method == 'GET' or not LOCALHOST:
-        send_mail("SOLICITUD JOINITY", "El usuario "+str(request.GET.get("solicitaNombre", "Sinnombre")) +" y mail "+ str(request.GET.get("solicitaCorreo", "sinmail")) +" Solicita permanencia", "antoni@bueninvento.es", ["fernando@bueninvento.es"], fail_silently=False)
+    if not LOCALHOST:
+        send_mail("SOLICITUD JOINITY", "El usuario "+str(request.GET.get("solicitaNombre", "Sinnombre")) +" y mail "+ str(request.GET.get("solicitaCorreo", "sinmail")) +" Solicita permanencia", "joinity@joinity.com", ["golpesbajos@gmail.com", "borjadegregor@hotmail.com", "antoniespinosa@me.com"], fail_silently=False)
         return HttpResponseRedirect('/')
     else:
         return HttpResponseRedirect('/')
