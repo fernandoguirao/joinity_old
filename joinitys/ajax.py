f1ca01cf4363f10c0fab546ac77f6c2a2544ecc5
antoniespinosa <antoniespinosa@me.com>
2013-07-16 11:23:31 +0200

    Martes

--- joinitys/ajax.py
+++ joinitys/ajax.py
@@ -5,8 +5,8 @@ from django.template.loader import render_to_string
 @dajaxice_register
 def cargar_mas(request, categoria, n):
     #lista_aficiones=Joinitys.objects.filter(tipo="3")[n:n+8]
-    lista_aficiones=Joinitys.objects.filter(tipo="3")
-    aficiones=render_to_string('index/ajax_aficiones.html', {"lista_aficiones":lista_aficiones, "cinco":[1,2,3,4,5]})
-    n=lista_aficiones.count()+n
+    lista_joinitys=Joinitys.objects.filter(tipo=categoria)[n:n+8]
+    aficiones=render_to_string('index/ajax_lista_joinitys.html', {"lista_joinitys":lista_joinitys, "cinco":[1,2,3,4,5]})
+    n=lista_joinitys.count()+n
     return simplejson.dumps({'aficiones':aficiones, 'n':n})
 
