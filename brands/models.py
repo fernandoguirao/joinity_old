82a8708d0faaa11b944d83c25a8dbbd966dcc32d
antoniespinosa <antoniespinosa@me.com>
2013-07-28 22:01:15 +0200

    prebrands

--- brands/models.py
+++ brands/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 class Brand(models.Model):
     nombre=models.TextField()
-    notas=models.TextField(null=True, max_length=400)
-    precio=models.IntegerField(default=0)
+    direccion=models.TextField()
+    web=models.TextField()
     tipo=models.TextField(null=True, max_length=100)
     calificacion=models.IntegerField(default=0)
     admin=models.ForeignKey(User, related_name="brand_admin")
     clase=models.IntegerField(default=0)
     ubicacion=models.TextField()
-    imagen_cabecera=models.ImageField(max_length=100, upload_to='brands', blank=True, null=True)
+    imagen_cabecera=models.ImageField(max_length=100, upload_to='brands', blank=True, null=True, default="defaults/avatar.jpeg")
     video=models.TextField(blank=True, null=True)
-    imagen_fondo=models.ImageField(max_length=100, upload_to='brands', blank=True, null=True)
+    imagen_fondo=models.ImageField(max_length=100, upload_to='brands', blank=True, null=True, default="defaults/avatar.jpeg")
     seguidores=models.ManyToManyField(User)
+    telefono=models.IntegerField(blank=True, null=True)
     
     class Meta:
         db_table = "Brands"
--- brands/urls.py
+++ brands/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from brands import views
 urlpatterns = patterns('',
-    url(r'^(?P<nombre_brand>\d+)/$', views.ver, name='ver_brand'),
-    url(r'^$', views.ver, name='ver'),
+    url(r'^(?P<id_brand>\d+)/$', views.ver, name='ver_brand'),
+    url(r'^(?P<id_brand>\d+)/editar/$', views.editar, name='editar'),
 
 )
