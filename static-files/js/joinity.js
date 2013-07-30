361e60531453cb6dae5405c0aa7cbef42536219c
Fernando Guirao <fernando@bueninvento.es>
2013-07-30 10:18:14 +0200

    Cambios css y js

--- static-files/js/joinity.js
+++ static-files/js/joinity.js
@@ -565,10 +565,24 @@ $(function() {
   }
 })
 
-/* MOSTRAR FECHA FIN SI MÁS DE UN DÍA */
 
-$('.masdeundia').toggle(function(){
-  $('.diafin').addClass('mostrar');
-},function(){
-  $('.diafin').removeClass('mostrar');
+//============================================
+//! CREAR CHECKBOX QUE OCULTA Y MUESTRA DIVS
+//============================================
+
+
+function creaCheckbox (divinicio,leyenda,idcheck) {
+  enElDiv = $(divinicio);
+  enElDiv.hide();
+  idche = enElDiv.prev();
+  enElDiv.before('<label class="checkbox checkMuestra" for="'+idcheck+'"><input type="checkbox" value="" id="'+idcheck+'" data-toggle="checkbox"><span class="txtlabel">' + leyenda + '</span></label>');
+}
+
+creaCheckbox('#cuantos-participan','¿Hay limitación de participantes?','checknuevo');
+creaCheckbox('.diafin','¿Dura más de un día?','checktermina');
+
+$('.checkMuestra').click(function(){
+  $(this).next().slideDown();
 });
+
+/* Fin de crea checkbox */
