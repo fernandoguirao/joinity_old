ef823e43f3678529355dacecdd013387eb076f2a
antoniespinosa <antoniespinosa@me.com>
2013-07-31 19:07:45 +0200

    if del poder

--- paypal/standard/models.py
+++ paypal/standard/models.py
@@ -251,10 +251,6 @@ class PayPalStandardBase(Model):
             if self.is_transaction():
                 if self.payment_status not in self.PAYMENT_STATUS_CHOICES:
                     self.set_flag("Invalid payment_status. (%s)" % self.payment_status)
-                if duplicate_txn_id(self):
-                    self.set_flag("Duplicate txn_id. (%s)" % self.txn_id)
-                if self.receiver_email != RECEIVER_EMAIL:
-                    self.set_flag("Invalid receiver_email. (%s)" % self.receiver_email)
                 if callable(item_check_callable):
                     flag, reason = item_check_callable(self)
                     if flag:
